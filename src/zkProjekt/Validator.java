23a896f8968a9b04da5a36e72caec378adc98980
Michal <michalfrankowski1@gmail.com>
2013-01-25 11:02:14 +0100

    walidacja numeru rejestracyjnego

--- src/zkProjekt/Validator.java
+++ src/zkProjekt/Validator.java
@@ -1,5 +1,26 @@
 package zkProjekt;
 
-public class Validator {
+import java.util.Map;
 
+import org.zkoss.bind.Property;
+import org.zkoss.bind.ValidationContext;
+import org.zkoss.bind.validator.AbstractValidator;
+
+public class Validator extends AbstractValidator {
+	
+	public void validate(ValidationContext ctx) {
+		//all the bean properties
+		Map<String,Property> beanProps = ctx.getProperties(ctx.getProperty().getBase());
+		
+		//first let's check the passwords match
+		validateNumRej(ctx, (String)beanProps.get("numRej").getValue());
+	}
+
+
+	private void validateNumRej(ValidationContext ctx, String numRej) {	
+		if(numRej == null) {
+			this.addInvalidMessage(ctx, "numRej", "Podaj numer rejestracyjny!");
+		}
+	}
+	
 }
